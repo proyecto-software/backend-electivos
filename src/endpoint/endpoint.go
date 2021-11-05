c6e9ddcc4cdf45419dbc7cc9a10bff48512d7498
NicolasGarciaS <ngarcia1621@gmail.com>
2021-11-04 21:35:45 -0300

    ingreso de solicitud mediante el endpoint
    Endpoint formulario listo

--- src/endpoint/endpoint.go
+++ src/endpoint/endpoint.go
@@ -19,7 +19,53 @@ func Formulario(c *gin.Context, db *sql.DB, logger *logrus.Entry) (data models.F
 		return
 	} else {
 		//TODO EL CODIGO PARA BD
+		var solicitud models.Solicitud
+
 		Alumno := function.Alumno_info(db, data.Rut)
+		solicitud.Id_alumno = Alumno.Id
+		//deberia ser un serial
+		solicitud.Id = 821
+		//Electivos
+		E1 := function.Electivo_info(db, data.Electivo1)
+		E2 := function.Electivo_info(db, data.Electivo2)
+		E3 := function.Electivo_info(db, data.Electivo2)
+
+		for i := 3; i < 6; i++ {
+			var postulacion models.Postulacion
+			//esto tampoco deberia estar
+			postulacion.Id = i * 456
+			//
+			postulacion.Aprobado = false
+			postulacion.Cantidad = 0
+			if i == 3 {
+				postulacion.Id_electivo = E1.Id
+				// AQUI VA UN SELECT POSTULACION RETURN ID.(AUTOINCREMENTO)
+				solicitud.Id_Postulacion_1 = postulacion.Id
+			}
+			if i == 4 {
+				postulacion.Id_electivo = E2.Id
+				// AQUI VA UN SELECT POSTULACION RETURN ID.(AUTOINCREMENTO)
+				solicitud.Id_Postulacion_2 = postulacion.Id
+			}
+			if i == 5 {
+				postulacion.Id_electivo = E3.Id
+				// AQUI VA UN SELECT POSTULACION RETURN ID.(AUTOINCREMENTO)
+				solicitud.Id_Postulacion_3 = postulacion.Id
+			}
+			//	id := function.Insert_postulacion(db, postulacion, logger)
+			/* if i == 3 {
+				solicitud.Id_Postulacion_1 = id
+			}
+			if i == 4 {
+				solicitud.Id_Postulacion_2 = id
+			}
+			if i == 5 {
+				solicitud.Id_Postulacion_3 = id
+			} */
+			function.Insert_postulacion(db, postulacion, logger)
+
+		}
+		function.Insert_solicitud(db, solicitud, logger)
 		logger.Info(Alumno.Nombre)
 		c.JSON(200, "OK")
 	}
--- src/function/sqlFunction.go
+++ src/function/sqlFunction.go
@@ -3,6 +3,9 @@ package function
 import (
 	"database/sql"
 	"electivos-ucn/src/models"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
 )
 
 func Administrador_info(db *sql.DB, rut string) (admin models.Administrador) {
@@ -65,8 +68,8 @@ func Carrera_info(db *sql.DB, rut string) (carrera models.Carrera) {
 
 }
 
-func Electivo_info(db *sql.DB, rut string) (electivo models.Electivo) {
-	rows, err := db.Query("SELECT * FROM public.electivo WHERE rut = $1 ", rut)
+func Electivo_info(db *sql.DB, nombre string) (electivo models.Electivo) {
+	rows, err := db.Query("SELECT * FROM public.electivo WHERE nombre = $1 ", nombre)
 	if err != nil {
 		panic(err)
 	}
@@ -143,4 +146,35 @@ func Solicitud_info(db *sql.DB, rut string) (solicitud models.Solicitud) {
 	}
 	return
 
+}
+
+func Insert_postulacion(db *sql.DB, postulacion models.Postulacion, logger *logrus.Entry) {
+	insertDynStmt := `INSERT INTO public.postulacion
+		VALUES ($1, $2, $3, $4);`
+	_, e := db.Exec(insertDynStmt, &postulacion.Id, &postulacion.Cantidad, &postulacion.Id_electivo, &postulacion.Aprobado)
+	if e != nil {
+		logger.Infof("Error postulacion")
+		recoverError()
+	} else {
+		logger.Infof("postulacion Cargada con Exito")
+	}
+}
+func Insert_solicitud(db *sql.DB, postulacion models.Solicitud, logger *logrus.Entry) {
+	insertDynStmt := `INSERT INTO public.solicitud
+		VALUES ($1, $2, $3, $4, $5);`
+	_, e := db.Exec(insertDynStmt, &postulacion.Id, &postulacion.Id_alumno, &postulacion.Id_Postulacion_1, &postulacion.Id_Postulacion_2, &postulacion.Id_Postulacion_3)
+	if e != nil {
+		logger.Infof("Error solicitud")
+		recoverError()
+	} else {
+		logger.Infof("solicitud Cargada con Exito")
+	}
+}
+func recoverError() {
+	defer func() {
+		if panicMessage := recover(); panicMessage != nil {
+			fmt.Printf("")
+		}
+	}()
+	panic("Error")
 }
