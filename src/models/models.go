33d7bddbbb0bbb24a5b6da8fb09597b53e5ced0e
NicolasGarciaS <ngarcia1621@gmail.com>
2021-11-15 17:32:19 -0300

    Update models.go

--- src/models/models.go
+++ src/models/models.go
@@ -24,37 +24,37 @@ type Profesor struct {
 }
 
 type Electivo struct {
-	Id          int    `json:"Id_Electivo"`
-	Nombre      string `json:"Nombre"`
-	NRC         string `json:"NRC"`
-	Id_profesor int    `json:"Id_profesor"`
+	Id          int    `json:"id_Electivo"`
+	Nombre      string `json:"nombre"`
+	NRC         string `json:"nrc"`
+	Id_profesor int    `json:"id_profesor"`
 }
 
 type Postulacion struct {
-	Id          int  `json:"Id_Postulacion"`
-	Cantidad    int  `json:"Cantidad_ramos"`
-	Id_electivo int  `json:"Id_Electivo"`
-	Aprobado    bool `json:"Aprovado"`
+	Id          int  `json:"id_Postulacion"`
+	Cantidad    int  `json:"cantidad_ramos"`
+	Id_electivo int  `json:"id_Electivo"`
+	Aprobado    bool `json:"aprovado"`
 }
 type Solicitud struct {
-	Id               int `json:"Id_Solicitud"`
-	Id_Postulacion_1 int `json:"Id_Postulacion_1"`
-	Id_Postulacion_2 int `json:"Id_Postulacion_2"`
-	Id_Postulacion_3 int `json:"Id_Postulacion_3"`
-	Id_alumno        int `json:"Id_alumno"`
+	Id               int `json:"id_Solicitud"`
+	Id_Postulacion_1 int `json:"id_Postulacion_1"`
+	Id_Postulacion_2 int `json:"id_Postulacion_2"`
+	Id_Postulacion_3 int `json:"id_Postulacion_3"`
+	Id_alumno        int `json:"id_alumno"`
 }
 type Alumno struct {
-	Id                  int    `json:"Id_alumno"`
-	Rut                 string `json:"Rut"`
-	Nombre              string `json:"Nombre"`
-	Correo              string `json:"Correo"`
-	Id_carrera          int    `json:"Id_carrera"`
-	Semestre_incompleto int    `json:"Semetre_incompleto"`
-	Cantidad_ramos      int    `json:"Cantidad_ramos"`
+	Id                  int    `json:"id_alumno"`
+	Rut                 string `json:"rut"`
+	Nombre              string `json:"nombre"`
+	Correo              string `json:"correo"`
+	Id_carrera          int    `json:"id_carrera"`
+	Semestre_incompleto int    `json:"semetre_incompleto"`
+	Cantidad_ramos      int    `json:"cantidad_ramos"`
 }
 type Carrera struct {
-	Id     int    `json:"Id_Carrera"`
-	Nombre string `json:"Nombre"`
+	Id     int    `json:"id_Carrera"`
+	Nombre string `json:"nombre"`
 }
 
 // User defines the user in db
