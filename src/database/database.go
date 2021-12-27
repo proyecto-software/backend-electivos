d34445b125dcefc247fe5e11114e7313981b69d3
NicolasGarciaSantelices <n.garciasantelices@gmail.com>
2021-12-27 17:27:37 -0300

    qaaasasa

--- src/database/database.go
+++ src/database/database.go
@@ -18,7 +18,7 @@ import (
 var GlobalDB *gorm.DB
 
 func DBConnection(logger *logrus.Entry) *sql.DB {
-	db, err := sql.Open("postgres", "host=ec2-52-72-252-211.compute-1.amazonaws.com port=5432 user=spdkbddxynexbe password=2244405d1de4d94e500c668868571ea324040709be8beb1ae2fc1769e311a91d dbname=d4qsaefpehhcjp sslmode=default")
+	db, err := sql.Open("postgres", "host=ec2-52-72-252-211.compute-1.amazonaws.com port=5432 user=spdkbddxynexbe password=2244405d1de4d94e500c668868571ea324040709be8beb1ae2fc1769e311a91d dbname=d4qsaefpehhcjp sslmode=require")
 
 	if err != nil {
 		log.Error("Error when try to connect db")
